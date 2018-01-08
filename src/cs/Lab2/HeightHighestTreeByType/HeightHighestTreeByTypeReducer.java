7d95b3260a29b87c0a680f448671090228b28b65
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:17:54 +0100

    Indentation

--- src/cs/Lab2/HeightHighestTreeByType/HeightHighestTreeByTypeReducer.java
+++ src/cs/Lab2/HeightHighestTreeByType/HeightHighestTreeByTypeReducer.java
@@ -8,33 +8,33 @@ import java.util.Iterator;
 public class HeightHighestTreeByTypeReducer extends Reducer<Text,FloatWritable,Text,FloatWritable> {
 	private FloatWritable heightHighestTreeByType = new FloatWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (treeType, list of Heights)
 	// Output : (treeType, highestHeight)
-    @Override
-    protected void reduce(final Text cleI, final Iterable<FloatWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        float highestHeight = 0;
-        float currentHeight;
-        
-        // For each tree 
-        Iterator<FloatWritable> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	// Recover the height
-        	currentHeight = iterator.next().get();
-        	
-        	// If the current height is higher than the current highest height, we update the information
-        	if (currentHeight > highestHeight)
-        	{
-        		highestHeight = currentHeight;
-        	}
-        }
-        
-        // Output
-        heightHighestTreeByType.set(highestHeight);
-        context.write(cleI,heightHighestTreeByType);
-    }
+    	@Override
+    	protected void reduce(final Text cleI, final Iterable<FloatWritable> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		float highestHeight = 0;
+		float currentHeight;
+
+		// For each tree 
+		Iterator<FloatWritable> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			// Recover the height
+			currentHeight = iterator.next().get();
+
+			// If the current height is higher than the current highest height, we update the information
+			if (currentHeight > highestHeight)
+			{
+				highestHeight = currentHeight;
+			}
+		}
+
+		// Output
+		heightHighestTreeByType.set(highestHeight);
+		context.write(cleI,heightHighestTreeByType);
+    	}
 
 }
