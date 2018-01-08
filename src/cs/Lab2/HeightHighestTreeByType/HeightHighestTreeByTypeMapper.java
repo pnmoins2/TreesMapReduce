95213f624a0846520a7b6f69c61baa2e32ae0203
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:18:45 +0100

    Indentation

--- src/cs/Lab2/HeightHighestTreeByType/HeightHighestTreeByTypeMapper.java
+++ src/cs/Lab2/HeightHighestTreeByType/HeightHighestTreeByTypeMapper.java
@@ -14,7 +14,7 @@ public class HeightHighestTreeByTypeMapper extends Mapper<LongWritable, Text, Te
 	// Output : (treeType, heightOfTree)
 	@Override
 	protected void map(LongWritable keyE, Text valE, Context context) throws IOException,InterruptedException
-    {
+    	{
 		// The first line of the the input file has no relevant information
 		if (keyE.get() != 0)
 		{
@@ -22,21 +22,21 @@ public class HeightHighestTreeByTypeMapper extends Mapper<LongWritable, Text, Te
 			String[] lineSplit = valE.toString().split(";");
 			
 			// If there is the theight of the tree
-        	if (!lineSplit[6].isEmpty())
-        	{
-        		// Recover the tree type
-        		typeTree.set(lineSplit[2]);
-        		
-        		// Recover the height of the tree
-        		height.set(Float.parseFloat(lineSplit[6]));
-        		
-        		// Output
-        		context.write(typeTree, height);
-        	}
-        	else
-        	{
-        		return;
-        	}
+			if (!lineSplit[6].isEmpty())
+			{
+				// Recover the tree type
+				typeTree.set(lineSplit[2]);
+
+				// Recover the height of the tree
+				height.set(Float.parseFloat(lineSplit[6]));
+
+				// Output
+				context.write(typeTree, height);
+			}
+			else
+			{
+				return;
+			}
+    		}
     	}
-    }
 }
