147f81ed6c24a83ec9471b23bd4abcd276d9bc1d
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:15:27 +0100

    Indentation

--- src/cs/Lab2/TreeCountByType/TreeCountByTypeReducer.java
+++ src/cs/Lab2/TreeCountByType/TreeCountByTypeReducer.java
@@ -8,25 +8,25 @@ import java.util.Iterator;
 public class TreeCountByTypeReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 	private IntWritable totalTreeCountByType = new IntWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (treeType, List of Numbers of Trees)
 	// Output : (treeType, treeCountByType)
-    @Override
-    protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        int sum = 0;
-        
-        // We count the number of trees
-        Iterator<IntWritable> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	sum += iterator.next().get();
-        }
-        
-        // Output
-        totalTreeCountByType.set(sum);
-        context.write(cleI,totalTreeCountByType);
-    }
+    	@Override
+    	protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		int sum = 0;
+
+		// We count the number of trees
+		Iterator<IntWritable> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			sum += iterator.next().get();
+		}
+
+		// Output
+		totalTreeCountByType.set(sum);
+		context.write(cleI,totalTreeCountByType);
+    	}
 
 }
